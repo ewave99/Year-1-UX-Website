bbd52b213df093458e18d174b5fe505622cd6e10
Earl Murdoch <ehmurdoch47@protonmail.com>
2024-12-13 16:25:57 +0000

    Change all links to relative, so that the site works properly in GitHub pages.

--- components.js
+++ components.js
@@ -24,10 +24,10 @@ class NavBar extends HTMLElement {
         this.innerHTML = `
             <nav class="navbar">
                 <ul>
-                    <li class="navbar-item"><a href="/index.html">Home</a></li>
-                    <li class="navbar-item"><a href="/about.html">About</a></li>
-                    <li class="navbar-item"><a href="/categories.html">Categories</a></li>
-                    <li class="navbar-item"><a href="/random.html">Random Recipe</a></li>
+                    <li class="navbar-item"><a href="index.html">Home</a></li>
+                    <li class="navbar-item"><a href="about.html">About</a></li>
+                    <li class="navbar-item"><a href="categories.html">Categories</a></li>
+                    <li class="navbar-item"><a href="random.html">Random Recipe</a></li>
                 </ul>
             </nav>
             `;
@@ -44,9 +44,9 @@ class Footer extends HTMLElement {
         this.innerHTML = `
              <footer class="footer">
                 <ul>
-                    <li class="footer-item"><a href="/privacy.html">Privacy Policy</a></li>
-                    <li class="footer-item"><a href="/terms.html">Terms of Service</a></li>
-                    <li class="footer-item"><a href="/contact.html">Contact</a></li>
+                    <li class="footer-item"><a href="privacy.html">Privacy Policy</a></li>
+                    <li class="footer-item"><a href="terms.html">Terms of Service</a></li>
+                    <li class="footer-item"><a href="contact.html">Contact</a></li>
                     <li class="footer-item">&copy; 2024 Food for Thought. All rights reserved.</li>
                 </ul>
             </footer>
